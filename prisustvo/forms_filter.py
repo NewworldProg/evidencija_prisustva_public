5e91f9ebd4e50fb29a9ef9cf619481d7721e4cd1
Klibibor <zenovicwarlock@gmail.com>
2025-05-28 13:33:12 +0200

    Obrisan forms_filter.py  forma premeštena u forms.py

--- prisustvo/forms_filter.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Uprava
-
-class FilterForma(forms.Form):
-    godina = forms.IntegerField(label="Godina", required=False)
-    mesec = forms.IntegerField(label="Mesec", required=False)
-    uprava = forms.ModelChoiceField(queryset=Uprava.objects.all(), required=False)
